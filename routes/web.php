0eb5cd83bc03a5a4f1c1e9f440d649c6a7c95537
nichoblina <nichoblina@gmail.com>
2022-05-26 22:10:42 +0800

    yup

--- routes/web.php
+++ routes/web.php
@@ -91,6 +91,7 @@ Route::view("/overtime-request-new", 'employee.overtime-request-new');
 Route::get("/overtime-request-edit/{id}", [OvertimeEmpController::class, "edit_overtime_request"]);
 Route::post("/overtime-request-created", [OvertimeEmpController::class, "add_overtime_request"]);
 Route::post("/overtime-request-edited", [OvertimeEmpController::class, "update_overtime_request"]);
+Route::post("/overtime-request-deleted", [OvertimeEmpController::class, "delete_overtime_request"]);
 
 /* Management */
 Route::view("/management", 'management.dashboard');
